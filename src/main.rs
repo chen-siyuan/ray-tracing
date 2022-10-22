24a8e5d0d5b3a2e4c37af8ad51adb879716ec46c
Siyuan Chen <siyuanc2@andrew.cmu.edu>
2022-10-22 02:07:42 -0400

    Implement surface normal (6.2)

--- src/main.rs
+++ src/main.rs
@@ -8,21 +8,35 @@ use crate::point3::Point3;
 use crate::ray::Ray;
 use crate::vec3::Vec3;
 
-fn hit_sphere(center: Point3, radius: f64, ray: &Ray) -> bool {
+fn hit_sphere(center: Point3, radius: f64, ray: &Ray) -> Option<Point3> {
     let co = ray.origin - center;
     let a = ray.direction * ray.direction;
-    let b = 2. * co * ray.direction;
+    let half_b = co * ray.direction;
     let c = co * co - radius * radius;
-    let determinant = b * b - 4. * a * c;
-    determinant > 0.
+    let determinant = half_b * half_b - a * c;
+    if determinant < 0. {
+        None
+    } else {
+        let t = (-half_b - f64::sqrt(determinant)) / a;
+        Some(ray.at(t))
+    }
 }
 
-fn ray_color(r: &Ray) -> Color {
-    if hit_sphere(Point3(0., 0., -1.), 0.5, r) {
-        return Color(1.0, 0., 0.);
+fn ray_color(ray: &Ray) -> Color {
+    let center = Point3(0., 0., -1.);
+    let radius = 0.5;
+
+    match hit_sphere(center, radius, ray) {
+        Some(intersection) => {
+            let normal = intersection - center;
+            let unit_normal = normal.normalize();
+            (unit_normal + Vec3(1., 1., 1.)) / 2.
+        }
+        None => {
+            let unit_direction = ray.direction.normalize();
+            Color(1.0, 1.0, 1.0).interpolate(Color(0.5, 0.7, 1.0), (-unit_direction.0 + 1.) / 2.)
+        }
     }
-    let unit_direction = r.direction.normalize();
-    Color(1.0, 1.0, 1.0).interpolate(Color(0.5, 0.7, 1.0), (-unit_direction.0 + 1.) / 2.)
 }
 
 fn main() {
@@ -48,12 +62,12 @@ fn main() {
         for j in 0..image_width {
             let u = i as f64 / (image_height - 1) as f64;
             let v = j as f64 / (image_width - 1) as f64;
-            let r = Ray {
+            let ray = Ray {
                 origin,
                 direction: -origin + upper_left_corner + u * vertical + v * horizontal,
             };
-            let c = ray_color(&r);
-            write_color(&c);
+            let color = ray_color(&ray);
+            write_color(&color);
         }
     }
     eprintln!("\rLines remaining: {} ", 0);
