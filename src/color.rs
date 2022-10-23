f14af1040c81c048cc625f8568ea7b6b75cf75c3
Siyuan Chen <siyuanc2@andrew.cmu.edu>
2022-10-22 22:02:03 -0400

    Implement gamma correction (8.3)

--- src/color.rs
+++ src/color.rs
@@ -5,6 +5,10 @@ const MAX_COLOR: f64 = 255.999;
 pub fn write_color(color: &Color) -> () {
     let Color(r, g, b) = color;
 
+    let r = f64::sqrt(*r);
+    let g = f64::sqrt(*g);
+    let b = f64::sqrt(*b);
+
     let ir = (MAX_COLOR * r).floor() as i32;
     let ig = (MAX_COLOR * g).floor() as i32;
     let ib = (MAX_COLOR * b).floor() as i32;
